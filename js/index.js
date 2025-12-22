5e46af7f682cb01e0f00e9ea14db2ccb74c1f4ae
mysticdicc <richardfosterwork@gmail.com>
2025-12-22 00:25:39 +0000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function onClickExpandWorkExperience() { 
+    var weTableWrapper = document.getElementById("we_table_wrapper");
+
+    if (weTableWrapper.style.maxHeight === '0px') {
+        weTableWrapper.style.maxHeight = weTableWrapper.scrollHeight + 'px';
+    } else {
+        weTableWrapper.style.maxHeight = '0px';
+    }
+}
