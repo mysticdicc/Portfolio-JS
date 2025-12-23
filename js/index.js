5b8e33e7953102a28d705a44439e7d2ddeff635d
mysticdicc <richardfosterwork@gmail.com>
2025-12-23 00:20:49 +0000

    Update index.js
    re added accidentally deleted method

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import '../styles/web.css';
 
 window.onload = onIndexLoad;
+document.getElementById("we_expand_button").onclick = onClickExpandWorkExperience;
 
 function onIndexLoad() {
     var profile_bw = document.getElementById("profile_photo");
@@ -45,4 +46,13 @@ function setSvgColorFadeIn(element, color) {
     element.style.transition = "fill 0.3s ease-in-out, stroke 0.3s ease-in-out";
     element.style.fill = "var(--" + color + ") !important";
     element.style.stroke = "var(--" + color + ") !important";
+}
+
+function onClickExpandWorkExperience() { 
+    var weTableWrapper = document.getElementById("we_table_wrapper");
+    if (weTableWrapper.style.maxHeight === '0px') {
+        weTableWrapper.style.maxHeight = weTableWrapper.scrollHeight + 'px';
+    } else {
+        weTableWrapper.style.maxHeight = '0px';
+    }
 }
